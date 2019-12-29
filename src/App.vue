8a539f1979e7d46224ea00306af79199a205fc14
Shrish <b.shrish12@gmail.com>
2019-12-29 12:50:51 +1100

    add home view

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Address Book</b-navbar-brand><b-navbar-toggle target="nav-collapse"></b-navbar-toggle><b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item to="/" :active="path  == '/'">Home</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <router-view />
   </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
+</template><script>
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      path: this.$route && this.$route.path
+    };
+  },
+  watch: {
+    $route(route) {
+      this.path = route.path;
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.page {
+  padding: 20px;
+}button {
+  margin-right: 10px;
 }
 </style>
